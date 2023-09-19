29b79661f1b884b351625b08556f46d1e2003901
Innocent Josiah Patrick <innocentjosiah57@gmail.com>
2023-09-19 10:24:18 +0100

    trying to host on vercel

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/3dPortfolio/", 
+  base: './', // Set the base path to the root of your application
   build: {
     rollupOptions: {
-      external: ["three"],
+      external: ['three'],
     },
   },
 });
